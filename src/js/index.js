312a835ed7b20439353c1433293592296a11b3c4
Hamilton Godoi <ha1000tongs@gmail.com>
2023-03-21 10:32:05 -0300

    UPDATE 03

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,18 @@
-// alert('Ola Hamilton Godoi')
-// console.log('Hamilton');
-// console.log(document);
 
-/*
-Objetivo: quando clicarmos no botão temos que mostrar a imagem de fundo correspondente.
-*/
-
-// - passo 1 - dar um jeito de pegar o elemento HTML dos botões
 const botoesCarrossel = document.querySelectorAll('.botao');
 const imagens = document.querySelectorAll('.imagem')
 
-// - passo 2 - dar um jeito de identificar o clique do usuário no botão
 botoesCarrossel.forEach((botao, indice) => {
-    // console.log(botao);
-    botao.addEventListener('click', () => {
-        // console.log('clicou no botao');
 
-        // - passo 3 - desmarcar o botão selecionado anterior
+    botao.addEventListener('click', () => {
+
         desativarBotaoSelecionado();
 
-        // - passo 4 - marcar o botão clicado como se estivesse selecionado
         selecionaBotaoCarrossel(botao);
 
-        // - passo 5 - esconder a imagem "ativa" de anterior
         esconderImagemAtiva();
-        // console.log('ativa');
 
-        // - passo 6 - fazer aparecer a imagem correspondente ao botão clicado
         mostrarImagemDeFundo(indice);
-        // console.log(imagens);
-        // console.log(imagens[1]);
-
     })
 })
 
@@ -52,10 +34,6 @@ function desativarBotaoSelecionado() {
     const botaoSelecionado = document.querySelector('.selecionado');
     botaoSelecionado.classList.remove('selecionado');
 }
-// const botao1 = document.getElementById("botao1")
-// botao1.addEventListener('click', () => {
-//     console.log('clicou no botão 1');
-// })
 
 
 
